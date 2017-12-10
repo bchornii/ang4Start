2f1a3a437b5ef76e25f05b4498332a33ac6d8696
bchornii <bogdan.chorniy@gmail.com>
2017-12-10 14:50:47 +0200

    Added ngFor into app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,22 @@ export class AppComponent {
   };
   courses = [1, 2, 3];
   viewMode = 'map';
+  courses2 = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'}
+  ];
 
   onFavoriteChange(obj: FavoriteChangedEventArgs){
     console.log(obj.msg);
+  }
+
+  onAddCourse(){
+    let newId = this.courses2[(this.courses2.length - 1)].id + 1;
+    this.courses2.push({id: newId, name: 'course' + newId})
+  }
+
+  onRemoveCourse(course){
+    let index = this.courses2.indexOf(course);
+    this.courses2.splice(index, 1);
   }
 }
