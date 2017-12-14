008747ae8d82b7ea1c286939d6eb4e769d9cc640
bchornii <bogdan.chorniy@gmail.com>
2017-12-14 23:31:51 +0200

    Added basic validation to input fields along with error information. Added validation styles

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -2,14 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'contact-form',
-  templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.css']
+  templateUrl: './contact-form.component.html'
 })
 export class ContactFormComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  log(el){
+    console.log(el);
   }
 
 }
