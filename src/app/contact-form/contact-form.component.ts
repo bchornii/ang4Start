e700cea259f25ff1cb64b517d7d395c7a91a257e
bchornii <bogdan.chorniy@gmail.com>
2017-12-16 10:49:59 +0200

    Added drop down list

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -6,17 +6,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
   isChecked: boolean;
+  contactMethods = [
+    {id: 1, name: 'Email'},
+    {id: 2, name: 'Phone'}
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+
   }
 
   log(el){
     console.log(el);
   }
 
-  onSubmit(f){
+  onSubmit(f, $event){
+    $event.preventDefault();
+    let formInputControls = f.controls;
+    let select = formInputControls.contactMethod.value > 0
+      ? formInputControls.contactMethod.value
+      : 0;
+    console.log('select = ' + select)
     console.log(f)
   }
 
