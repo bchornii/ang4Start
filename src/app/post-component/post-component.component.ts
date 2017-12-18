039d1f459934a58e6f648e6e263285fb510786de
bchornii <bogdan.chorniy@gmail.com>
2017-12-18 23:10:52 +0200

    Added post method to post component.

--- src/app/post-component/post-component.component.ts
+++ src/app/post-component/post-component.component.ts
@@ -7,12 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './post-component.component.html'
 })
 export class PostComponentComponent {
+  private url = "http://jsonplaceholder.typicode.com/posts";
   posts: any[];
 
   constructor(private http: Http){
-    http.get('http://jsonplaceholder.typicode.com/posts')
+    this.http.get(this.url)
         .subscribe(response => {
           this.posts = response.json();
+        });
+  }
+
+  createPost(input: HTMLInputElement){
+    let post = {
+      title: input.value
+    };
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+        .subscribe(response => {
+          post['id'] = response.json().id;
+          this.posts.splice(0, 0, post);
         });
   }
 }
