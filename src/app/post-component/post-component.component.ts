589255504a0b9f3fa7f5cf3f47df938453064355
bchornii <bogdan.chorniy@gmail.com>
2017-12-18 23:19:39 +0200

    Added patch(put) method to post component

--- src/app/post-component/post-component.component.ts
+++ src/app/post-component/post-component.component.ts
@@ -28,4 +28,11 @@ export class PostComponentComponent {
           this.posts.splice(0, 0, post);
         });
   }
+
+  updatePost(post){
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead: true }))
+        .subscribe(response => {
+          console.log(response.json());
+        })
+  }
 }
